74d5a189a552eabe1a18263af39b81b2facada47
keMoleseng <mmokgosi9@gmail.com>
2022-06-25 22:57:59 +0200

    including about section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Treatment from './components/Treatment';
 import Staff from './components/Staff';
+import About from './components/About';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <Treatment />
         <Staff />
+        <About />
     </div>
   );
 }
