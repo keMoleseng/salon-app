4a5301fb263f80ce669817f34e0c817bdd48e41b
keMoleseng <mmokgosi9@gmail.com>
2022-06-29 10:14:40 +0200

    adding functionality to contact form

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,24 +1,60 @@
-import '../contact.css'
+import '../contact.css';
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function ContactUs(){
+    const form = useRef();
+
+    const SERVICE_ID = 'service_x3aurgm';
+    const TEMPLATE_ID = 'template_1t9bxim';
+    const PUBLIC_KEY = 'krMgm_CqfiR1Q8z8N';
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+            e.target.reset();
+    };
+
     return(
         <div className="contact--container">
             <h1 className="contact--heading">Contact Us</h1>
             <span>
                 <span className="address"></span>
                 <span className="form--container">
-                    <form name="subscription">
-                        <input id="name" 
-                                type="text" 
-                                name="name"
-                                placeholder='Name'
-                                />
-                        <input id='email'
-                                type="email"
-                                placeholder="Email"
+                    <form ref={form} onSubmit={sendEmail}>
                         
-                        />
-
+                            <input 
+                                    type="text" 
+                                    name="user_name"
+                                    placeholder='Name'
+                                    className='name'
+                                    required
+                                    />
+                            <input 
+                                    type="email"
+                                    name="user_email"
+                                    placeholder="Email"
+                                    className='email'
+                                    required
+                            />
+                       
+                        <textarea id="opinion"
+                                    name="message"
+                                    placeholder='Message'
+                                    form='contactUs'
+                                    className='textarea'
+                                    required
+                        /> 
+                        <button id ="btn" type='submit' className='submit-btn'>
+                            Send
+                        </button>
                     </form>
                 </span>
             </span>
