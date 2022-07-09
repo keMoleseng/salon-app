c30f848cac8b1ee6984b39dd5ac191411d978565
keMoleseng <mmokgosi9@gmail.com>
2022-07-09 17:45:20 +0200

    Removing Header component, adding elements to nav bar and adding ternary operator to nav to change from home page to booking page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,36 @@
-import styles from '../styles/header.module.css'
+import styles from '../styles/header.module.css';
+import Link from 'next/link';
 
-export default function Navbar() {
+export default function Navbar({home}) {
     return(
-        <>
-            <ul className='nav' >
-                <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#hero">Home</a></li>
-                <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#treatmemts">Treatments</a></li>
-                <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#our_staff">Our Staff</a></li>
-                <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#about_us">About Us</a></li>
-                <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#contact_us">Contact Us</a></li>
-            </ul>
-        </>
+        <div className={styles.header}>
+            <div className={styles.heading_container}>
+                <h1 className={styles.ownerName}>Trevor Sorbie</h1>
+            </div>
+            { home ?(
+
+                <ul className={styles.nav} >
+                    <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#hero">Home</a></li>
+                    <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#treatmemts">Treatments</a></li>
+                    <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#our_staff">Our Staff</a></li>
+                    <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#about_us">About Us</a></li>
+                    <li className={styles.nav_item}><a className={styles.nav_itemConnection} href="#contact_us">Contact Us</a></li>
+                </ul>
+            )
+            :
+            (
+                <ul className={styles.nav} >
+                    <li>
+                        <Link href='/'>
+                        <li className={styles.nav_item}>
+                            <i className='fas fa-angle-double-left'/>
+                            <a className={styles.nav_itemConnection}> Take Me Home</a>
+                        </li>
+                        </Link>
+                    </li>
+                </ul>
+            )
+        }
+        </div>
     )
 }
