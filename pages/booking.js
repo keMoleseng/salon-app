a96c007b86e12e0546dcfefdba8e695fdc525c8a
keMoleseng <mmokgosi9@gmail.com>
2022-07-09 14:37:32 +0200

    Adding booking page

---
+++ pages/booking.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Booking() {
+    return (
+    <div>
+        <h1>Book Here</h1>
+        <Link href='/'>
+            <a>Home</a>
+        </Link>
+    </div>)
+}
