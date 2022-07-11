d163bf25168eb1ef1ce1ea468e3e8f9b5cb43756
keMoleseng <mmokgosi9@gmail.com>
2022-07-11 22:11:59 +0200

    Installing calendar npm and adding calendar component to booking.js page and adding styling. As well as fixing styling on Navbar.

---
+++ components/Card.js
@@ -0,0 +1,13 @@
+export default function Card() {
+    return (
+        <div>
+            <h4>Heading</h4>
+            <p>image</p>
+            <h5>Pricing</h5>
+            <p>info</p>
+            <p>info</p>
+            <p>info</p>
+            <span>Book Now</span>
+        </div>
+    )
+}
--- pages/booking.js
+++ pages/booking.js
@@ -1,10 +1,13 @@
-import Link from 'next/link';
+import React, { Suspense } from 'react';
+import Calendar from 'react-calendar';
 import Navbar from '../components/Navbar';
+
 
 export default function Booking() {
     return (
     <div>
-        <Navbar />
-        <h1>STUFFS</h1>
-    </div>)
+      <Navbar />
+      <Calendar />
+    </div>
+    )
 }
