df1b2b98ab1076a6613d896c87dcb388a44b364a
keMoleseng <mmokgosi9@gmail.com>
2022-07-15 18:38:45 +0200

    edits to .next folder and webpack.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03b86ed45ebdbf9f"; }
+/******/ 		__webpack_require__.h = function() { return "4480c03be5051107"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
