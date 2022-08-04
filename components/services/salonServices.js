a62e7dc0c3f2cf075b4f9b292bae6314bdb4e499
keMoleseng <mmokgosi9@gmail.com>
2022-08-04 16:03:47 +0200

    Adding salonServices component to handle data

---
+++ components/services/salonServices.js
@@ -0,0 +1,11 @@
+export const getTreatments = () => ([
+    { id: '1', title: "Hair" },
+    { id: '2', title: "MakeUp" },
+    { id: '3', title: "Spa"}
+])
+
+export const getBeauticians = () => ([
+    { id: '1', title: "Lebo" },
+    { id: '2', title: "Lucy" },
+    { id: '3', title: "Lerato" }
+])
